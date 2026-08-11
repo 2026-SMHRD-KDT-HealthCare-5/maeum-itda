b2ec07b1462c25fbc2fcd71c971a41050b4d238a
PHS <haexunx@gmail.com>
2026-08-11 18:01:18 +0900

    feat: 보호자 알림함 목록/읽음 처리 구현

--- apps/frontend/src/pages/guardian-notification/ui/GuardianNotificationPage.tsx
+++ apps/frontend/src/pages/guardian-notification/ui/GuardianNotificationPage.tsx
@@ -1,13 +1,16 @@
 import { MarkNotificationReadAction } from '../../../features/mark-notification-read'
 import { BottomTabBar, GUARDIAN_TAB_ITEMS } from '../../../widgets/bottom-tab-bar'
+import styles from './GuardianNotificationPage.module.css'
 
 // GUARDIAN_NOTIFICATION_01 (UC-11)
 export function GuardianNotificationPage() {
   return (
-    <main>
-      <h1>알림함</h1>
-      <MarkNotificationReadAction />
+    <>
+      <main className={styles.page}>
+        <h1 className={styles.title}>알림</h1>
+        <MarkNotificationReadAction />
+      </main>
       <BottomTabBar items={GUARDIAN_TAB_ITEMS} />
-    </main>
+    </>
   )
 }
